4a2cf5766fe9dfd7f37de09181355087c99a1a3a
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-16 06:55:27 +0700

    Create ParallaxGenerator.cpp

---
+++ src/ParallaxGenerator.cpp
@@ -0,0 +1,30 @@
+#include "ParallaxGenerator.hpp"
+#include "EditorLayerBridge.hpp"
+
+void ParallaxGenerator::generateBackgroundCity() {
+    auto editor = EditorLayerBridge::editor;
+    if (!editor) return;
+
+    // TODO:
+}
+
+void ParallaxGenerator::generateFarGlow() {
+    auto editor = EditorLayerBridge::editor;
+    if (!editor) return;
+
+    // TODO:
+}
+
+void ParallaxGenerator::generateNearGlow() {
+    auto editor = EditorLayerBridge::editor;
+    if (!editor) return;
+
+    // TODO:
+}
+
+void ParallaxGenerator::generateForegroundDetail() {
+    auto editor = EditorLayerBridge::editor;
+    if (!editor) return;
+
+    // TODO:
+}
