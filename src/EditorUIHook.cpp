f0047d926541b1e7a085ae2c5ab6088f53c098b0
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-15 20:39:43 +0700

    Update EditorUIHook.cpp

--- src/EditorUIHook.cpp
+++ src/EditorUIHook.cpp
@@ -8,23 +8,36 @@ bool MyEditorUI::init(LevelEditorLayer* editor) {
     if (!EditorUI::init(editor))
         return false;
 
+    // Save editor reference
     EditorLayerBridge::editor = editor;
 
+    auto sprite =
+        CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png");
+
     auto btn = CCMenuItemSpriteExtra::create(
-        CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png"),
+        sprite,
         this,
         menu_selector(MyEditorUI::onDeco)
     );
 
+    btn->setScale(1.1f);
     btn->setPosition({220.f, -90.f});
 
     if (auto menu = this->getChildByType<CCMenu>(0)) {
         menu->addChild(btn);
     }
 
+    log::info("Auto Deco Kuzureta loaded");
+
     return true;
 }
 
 void MyEditorUI::onDeco(CCObject*) {
+    FLAlertLayer::create(
+        "Auto Deco",
+        "Generating Kuzureta Style...",
+        "OK"
+    )->show();
+
     KuzuretaGenerator::generate();
 }
