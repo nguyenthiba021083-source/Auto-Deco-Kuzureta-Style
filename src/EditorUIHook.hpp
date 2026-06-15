bc485efcbc6e95c936a6e1d0056633401d543c59
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-15 14:45:09 +0700

    Create EditorUIHook.hpp

---
+++ src/EditorUIHook.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <Geode/modify/EditorUI.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyEditorUI, EditorUI) {
+public:
+    bool init(LevelEditorLayer* editor);
+};
