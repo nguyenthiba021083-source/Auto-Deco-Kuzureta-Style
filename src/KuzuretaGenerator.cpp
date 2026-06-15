fa54d851ad9e75aad12a2fdfe090969027949057
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-15 15:05:56 +0700

    Update KuzuretaGenerator.cpp

--- src/KuzuretaGenerator.cpp
+++ src/KuzuretaGenerator.cpp
@@ -1,1 +1,19 @@
-// generator
+#include "KuzuretaGenerator.hpp"
+
+void KuzuretaGenerator::generate() {
+    auto editor = LevelEditorLayer::get();
+
+    if (!editor)
+        return;
+
+    float startX = 1000.f;
+    float y = 150.f;
+
+    for (int i = 0; i < 10; i++) {
+        auto obj = editor->createObject(1, { startX + i * 30.f, y }, false);
+        
+        if (obj) {
+            editor->addObject(obj);
+        }
+    }
+}
