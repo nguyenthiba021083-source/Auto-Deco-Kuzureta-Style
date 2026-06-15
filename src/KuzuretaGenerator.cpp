b6644dfbda764617594aeb45bacc775fce83d15d
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-15 19:43:01 +0700

    Update KuzuretaGenerator.cpp

--- src/KuzuretaGenerator.cpp
+++ src/KuzuretaGenerator.cpp
@@ -1,40 +1,74 @@
 #include "KuzuretaGenerator.hpp"
+#include "LayoutAnalyzer.hpp"
 
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
+constexpr int ID_GLOW_CIRCLE   = 1006;
+constexpr int ID_GLOW_EDGE     = 915;
+
+constexpr int ID_CHAIN_A       = 102;
+constexpr int ID_CHAIN_B       = 103;
+
+constexpr int ID_LINE_THIN     = 1613;
+
+constexpr int ID_CRYSTAL_A     = 1732;
+constexpr int ID_CRYSTAL_B     = 1733;
+
+constexpr int ID_CITY_BLOCK    = 211;
+constexpr int ID_WINDOW_LIGHT  = 673;
+
 void KuzuretaGenerator::generate() {
 
-    log::info("========== Auto-Deco-Kuzureta ==========");
+    auto stats = LayoutAnalyzer::analyze();
 
-    log::info("Glow Circle ID: {}", KuzuretaIDs::GLOW_CIRCLE);
-    log::info("Glow Edge ID: {}", KuzuretaIDs::GLOW_EDGE);
+    log::info("========== Kuzureta Generator ==========");
+    log::info("Total Objects: {}", stats.totalObjects);
+    log::info("Level Length: {}", stats.levelLength);
 
-    log::info("Chain A ID: {}", KuzuretaIDs::CHAIN_A);
-    log::info("Chain B ID: {}", KuzuretaIDs::CHAIN_B);
-
-    log::info("Line ID: {}", KuzuretaIDs::LINE_THIN);
-
-    log::info("Crystal A ID: {}", KuzuretaIDs::CRYSTAL_A);
-    log::info("Crystal B ID: {}", KuzuretaIDs::CRYSTAL_B);
-
-    log::info("City Block ID: {}", KuzuretaIDs::CITY_BLOCK);
-    log::info("Window Light ID: {}", KuzuretaIDs::WINDOW_LIGHT);
-
-    log::info("Generating Kuzureta Style...");
-
-    for (int i = 0; i < 200; i++) {
-        log::debug("Glow {}", i);
+    if (stats.totalObjects < 100) {
+        log::info("Layout Size: Small");
+    }
+    else if (stats.totalObjects < 1000) {
+        log::info("Layout Size: Medium");
+    }
+    else {
+        log::info("Layout Size: Large");
     }
 
-    for (int i = 0; i < 80; i++) {
-        log::debug("Crystal {}", i);
+    log::info("Cube Count: {}", stats.cubeCount);
+    log::info("Ship Count: {}", stats.shipCount);
+    log::info("Ball Count: {}", stats.ballCount);
+    log::info("UFO Count: {}", stats.ufoCount);
+    log::info("Wave Count: {}", stats.waveCount);
+
+    // Kuzureta preset statistics
+
+    int glowCount = 200;
+    int crystalCount = 80;
+    int chainCount = 50;
+    int cityCount = 40;
+    int windowCount = 120;
+
+    if (stats.totalObjects < 100) {
+        glowCount = 80;
+        crystalCount = 30;
+        chainCount = 20;
+        cityCount = 15;
+        windowCount = 40;
     }
 
-    for (int i = 0; i < 50; i++) {
-        log::debug("Chain {}", i);
-    }
+    log::info("Glow Objects: {}", glowCount);
+    log::info("Crystal Objects: {}", crystalCount);
+    log::info("Chain Objects: {}", chainCount);
+    log::info("City Objects: {}", cityCount);
+    log::info("Window Objects: {}", windowCount);
 
-    log::info("Generation Finished");
+    log::info("Using Glow ID: {}", ID_GLOW_CIRCLE);
+    log::info("Using Crystal ID: {}", ID_CRYSTAL_A);
+    log::info("Using Chain ID: {}", ID_CHAIN_A);
+    log::info("Using City ID: {}", ID_CITY_BLOCK);
+
+    log::info("Kuzureta Analysis Complete");
 }
