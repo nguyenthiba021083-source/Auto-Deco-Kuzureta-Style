4ccd84f53ea51c4922e2bcab915ec9b740d9db5b
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-16 06:53:14 +0700

    Create TriggerGenerator.hpp

---
+++ src/TriggerGenerator.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class TriggerGenerator {
+public:
+    static void generatePulseTriggers();
+    static void generateMoveTriggers();
+    static void generateAlphaTriggers();
+    static void generateColorTriggers();
+};
