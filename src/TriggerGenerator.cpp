e12f2dd9cdcd8594902d3d40262473c276914ef9
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-16 06:53:51 +0700

    Create TriggerGenerator.cpp

---
+++ src/TriggerGenerator.cpp
@@ -0,0 +1,34 @@
+#include "TriggerGenerator.hpp"
+#include "EditorLayerBridge.hpp"
+
+void TriggerGenerator::generatePulseTriggers() {
+    auto editor = EditorLayerBridge::editor;
+    if (!editor) return;
+
+    // TODO:
+    // Spawn Pulse Triggers
+}
+
+void TriggerGenerator::generateMoveTriggers() {
+    auto editor = EditorLayerBridge::editor;
+    if (!editor) return;
+
+    // TODO:
+    // Spawn Move Triggers
+}
+
+void TriggerGenerator::generateAlphaTriggers() {
+    auto editor = EditorLayerBridge::editor;
+    if (!editor) return;
+
+    // TODO:
+    // Spawn Alpha Triggers
+}
+
+void TriggerGenerator::generateColorTriggers() {
+    auto editor = EditorLayerBridge::editor;
+    if (!editor) return;
+
+    // TODO:
+    // Spawn Color Triggers
+}
