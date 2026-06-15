8eb033df4254039e13ce17f0fab9f3b9f5f9f4d9
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-15 15:44:10 +0700

    Update EditorUIHook.cpp

--- src/EditorUIHook.cpp
+++ src/EditorUIHook.cpp
@@ -6,7 +6,17 @@ bool MyEditorUI::init(LevelEditorLayer* editor) {
         return false;
     }
 
-    KuzuretaGenerator::generate();
+    auto btn = CCMenuItemSpriteExtra::create(
+        CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png"),
+        this,
+        menu_selector(MyEditorUI::onDeco)
+    );
+
+    btn->setPosition({220.f, -90.f});
+
+    if (auto menu = this->getChildByType<CCMenu>(0)) {
+        menu->addChild(btn);
+    }
 
     return true;
 }
