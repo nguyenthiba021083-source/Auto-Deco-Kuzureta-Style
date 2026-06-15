d78f7c6a400ec5226ae4f77a044a8ad132826e78
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-15 19:32:59 +0700

    Update KuzuretaGenerator.hpp

--- src/KuzuretaGenerator.hpp
+++ src/KuzuretaGenerator.hpp
@@ -3,9 +3,20 @@
 class KuzuretaGenerator {
 public:
     static void generate();
-
-private:
-    static void createGlow(float x, float y);
-    static void createCrystal(float x, float y);
-    static void createChain(float x, float y);
 };
+
+namespace KuzuretaIDs {
+    constexpr int GLOW_CIRCLE = 1006;
+    constexpr int GLOW_EDGE = 915;
+
+    constexpr int CHAIN_A = 102;
+    constexpr int CHAIN_B = 103;
+
+    constexpr int LINE_THIN = 1613;
+
+    constexpr int CRYSTAL_A = 1732;
+    constexpr int CRYSTAL_B = 1733;
+
+    constexpr int CITY_BLOCK = 211;
+    constexpr int WINDOW_LIGHT = 673;
+}
