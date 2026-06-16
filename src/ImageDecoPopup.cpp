6d3b2bdf3e6787d79740462650893df3ad68f08a
nguyenthiba021083 <nguyenthiba021083@users.noreply.github.com>
2026-06-17 04:28:59 +0700

    Add image path input

--- src/ImageDecoPopup.cpp
+++ src/ImageDecoPopup.cpp
@@ -1,13 +1,18 @@
 #include <Geode/Geode.hpp>
+#include <Geode/ui/TextInput.hpp>
+
 #include "ImageAnalyzer.hpp"
 
 using namespace geode::prelude;
 
 class ImageDecoPopup : public Popup<> {
+protected:
+    TextInput* m_input = nullptr;
+
 public:
     static ImageDecoPopup* create() {
         auto ret = new ImageDecoPopup();
-        if (ret && ret->initAnchored(260.f, 180.f)) {
+        if (ret && ret->initAnchored(320.f, 220.f)) {
             ret->autorelease();
             return ret;
         }
@@ -18,20 +23,36 @@ public:
     bool setup() override {
         this->setTitle("Build From Image");
 
+        m_input = TextInput::create(
+            260.f,
+            "Image Path"
+        );
+
+        m_input->setPosition({
+            m_size.width / 2,
+            m_size.height / 2 + 20.f
+        });
+
+        this->m_mainLayer->addChild(m_input);
+
         auto btn = CCMenuItemSpriteExtra::create(
-            ButtonSprite::create("Analyze"),
+            ButtonSprite::create("Build"),
             this,
             menu_selector(ImageDecoPopup::onAnalyze)
         );
 
         m_buttonMenu->addChild(btn);
-        btn->setPosition({0.f, 0.f});
+        btn->setPosition({0.f, -40.f});
 
         return true;
     }
 
     void onAnalyze(CCObject*) {
-        auto result = ImageAnalyzer::analyze("kuzureta");
+        std::string path =
+            m_input->getString();
+
+        auto result =
+            ImageAnalyzer::analyze(path);
 
         FLAlertLayer::create(
             "Image Build",
