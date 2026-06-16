a845c98ee2f9594cbad7533f199f3254a9714d13
nguyenthiba021083 <nguyenthiba021083@users.noreply.github.com>
2026-06-17 04:34:30 +0700

    Fix ImageDecoPopup build errors

--- src/ImageDecoPopup.cpp
+++ src/ImageDecoPopup.cpp
@@ -1,64 +1,23 @@
-#include "KuzuretaGenerator.hpp"
-#include "GlowGenerator.hpp"
-#include "CrystalGenerator.hpp"
-#include "LayoutAnalyzer.hpp"
-#include "StyleClassifier.hpp"
 #include <Geode/Geode.hpp>
-#include <Geode/ui/TextInput.hpp>
 
 #include "ImageAnalyzer.hpp"
+#include "StyleClassifier.hpp"
+#include "LayoutAnalyzer.hpp"
+
+#include "KuzuretaGenerator.hpp"
+#include "CrystalGenerator.hpp"
+#include "GlowGenerator.hpp"
 
 using namespace geode::prelude;
 
-class ImageDecoPopup : public Popup<> {
-protected:
-    TextInput* m_input = nullptr;
-
+class ImageDecoPopup : public FLAlertLayer {
 public:
-    static ImageDecoPopup* create() {
-        auto ret = new ImageDecoPopup();
-        if (ret && ret->initAnchored(320.f, 220.f)) {
-            ret->autorelease();
-            return ret;
-        }
-        delete ret;
-        return nullptr;
-    }
-
-    bool setup() override {
-        this->setTitle("Build From Image");
-
-        m_input = TextInput::create(
-            260.f,
-            "Image Path"
-        );
-
-        m_input->setPosition({
-            m_size.width / 2,
-            m_size.height / 2 + 20.f
-        });
-
-        this->m_mainLayer->addChild(m_input);
-
-        auto btn = CCMenuItemSpriteExtra::create(
-            ButtonSprite::create("Build"),
-            this,
-            menu_selector(ImageDecoPopup::onAnalyze)
-        );
-
-        m_buttonMenu->addChild(btn);
-        btn->setPosition({0.f, -40.f});
-
-        return true;
-    }
-
-    void onAnalyze(CCObject*) {
-
-        std::string path =
-            m_input->getString();
+    static void show() {
 
         auto analysis =
-            ImageAnalyzer::analyze(path);
+            ImageAnalyzer::analyze(
+                "/storage/emulated/0/Pictures/build.png"
+            );
 
         auto theme =
             StyleClassifier::classify(
@@ -69,27 +28,17 @@ public:
             LayoutAnalyzer::analyze();
 
         if (theme == "CRYSTAL") {
-
-            CrystalGenerator::generate(
-                stats
-            );
-
+            CrystalGenerator::generate(stats);
         }
         else if (theme == "GLOW") {
-
-            GlowGenerator::generate(
-                stats
-            );
-
+            GlowGenerator::generate(stats);
         }
         else {
-
             KuzuretaGenerator::generate();
-
         }
 
         FLAlertLayer::create(
-            "Build Complete",
+            "Build From Image",
             ("Theme: " + theme).c_str(),
             "OK"
         )->show();
