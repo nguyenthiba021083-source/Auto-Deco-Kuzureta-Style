10bd6c59c9c60cf5ccf588181f80f9380a5165dd
nguyenthiba021083 <nguyenthiba021083@users.noreply.github.com>
2026-06-17 14:44:26 +0700

    Restore image deco generation

--- src/ImageDecoPopup.cpp
+++ src/ImageDecoPopup.cpp
@@ -1,13 +1,37 @@
 #include "ImageDecoPopup.hpp"
 
+#include "ImageAnalyzer.hpp"
+#include "LayoutAnalyzer.hpp"
+#include "CrystalGenerator.hpp"
+#include "GlowGenerator.hpp"
+#include "KuzuretaGenerator.hpp"
+
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
 void ImageDecoPopup::open() {
+    auto imageResult =
+        ImageAnalyzer::analyze(
+            "/storage/emulated/0/Pictures/build.png"
+        );
+
+    auto stats =
+        LayoutAnalyzer::analyze();
+
+    if (imageResult.theme == "CRYSTAL") {
+        CrystalGenerator::generate(stats);
+    }
+    else if (imageResult.theme == "GLOW") {
+        GlowGenerator::generate(stats);
+    }
+    else {
+        KuzuretaGenerator::generate();
+    }
+
     FLAlertLayer::create(
-        "Auto Deco",
-        "Image Deco Popup Loaded",
+        "Build From Image",
+        ("Theme: " + imageResult.theme).c_str(),
         "OK"
     )->show();
 }
