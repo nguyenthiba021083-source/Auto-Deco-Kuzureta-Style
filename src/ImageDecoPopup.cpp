78ed26e0b7d91ace2ade97649a2a6410965024d2
nguyenthiba021083 <nguyenthiba021083@users.noreply.github.com>
2026-06-17 12:47:10 +0700

    Implement ImageDecoPopup open

--- src/ImageDecoPopup.cpp
+++ src/ImageDecoPopup.cpp
@@ -1,46 +1,13 @@
+#include "ImageDecoPopup.hpp"
+
 #include <Geode/Geode.hpp>
-
-#include "ImageAnalyzer.hpp"
-#include "StyleClassifier.hpp"
-#include "LayoutAnalyzer.hpp"
-
-#include "KuzuretaGenerator.hpp"
-#include "CrystalGenerator.hpp"
-#include "GlowGenerator.hpp"
 
 using namespace geode::prelude;
 
-class ImageDecoPopup : public FLAlertLayer {
-public:
-    static void open() {
-
-        auto analysis =
-            ImageAnalyzer::analyze(
-                "/storage/emulated/0/Pictures/build.png"
-            );
-
-        auto theme =
-            StyleClassifier::classify(
-                analysis
-            );
-
-        auto stats =
-            LayoutAnalyzer::analyze();
-
-        if (theme == "CRYSTAL") {
-            CrystalGenerator::generate(stats);
-        }
-        else if (theme == "GLOW") {
-            GlowGenerator::generate(stats);
-        }
-        else {
-            KuzuretaGenerator::generate();
-        }
-
-        FLAlertLayer::create(
-            "Build From Image",
-            ("Theme: " + theme).c_str(),
-            "OK"
-        )->show();
-    }
-};
+void ImageDecoPopup::open() {
+    FLAlertLayer::create(
+        "Build From Image",
+        "Popup Opened",
+        "OK"
+    )->show();
+}
