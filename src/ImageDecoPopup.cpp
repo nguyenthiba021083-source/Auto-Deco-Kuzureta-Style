491b8f4d27aa6a8c8cb8a1b011779771df7da4ee
nguyenthiba021083 <nguyenthiba021083@users.noreply.github.com>
2026-06-17 13:32:18 +0700

    Connect image analyzer to generators

--- src/ImageDecoPopup.cpp
+++ src/ImageDecoPopup.cpp
@@ -11,24 +11,25 @@
 using namespace geode::prelude;
 
 void ImageDecoPopup::open() {
+    std::string path =
+        "/storage/emulated/0/Pictures/build.png";
+
+    auto result = ImageAnalyzer::analyze(path);
+    auto stats = LayoutAnalyzer::analyze();
+
+    if (result.theme == "CRYSTAL") {
+        CrystalGenerator::generate(stats);
+    }
+    else if (result.theme == "GLOW") {
+        GlowGenerator::generate(stats);
+    }
+    else {
+        KuzuretaGenerator::generate();
+    }
+
     FLAlertLayer::create(
         "Build From Image",
-        "TODO: Nhap duong dan anh",
+        ("Theme: " + result.theme).c_str(),
         "OK"
     )->show();
-
-    // Ke hoach:
-    //
-    // std::string path =
-    // "/storage/emulated/0/Pictures/build.png";
-    //
-    // auto result = ImageAnalyzer::analyze(path);
-    // auto stats  = LayoutAnalyzer::analyze();
-    //
-    // if (result.theme == "CRYSTAL")
-    //     CrystalGenerator::generate(stats);
-    // else if (result.theme == "GLOW")
-    //     GlowGenerator::generate(stats);
-    // else
-    //     KuzuretaGenerator::generate();
 }
